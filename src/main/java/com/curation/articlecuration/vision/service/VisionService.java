1b8095f7a83c4c6cfacb998568660215bfd83c1e
Terry <terryakishin0814@gmail.com>
2022-10-19 13:43:32 +0900

    google vision API 사용 추가

---
+++ src/main/java/com/curation/articlecuration/vision/service/VisionService.java
@@ -0,0 +1,154 @@
+package com.curation.articlecuration.vision.service;
+
+import com.google.cloud.vision.v1.*;
+import com.google.protobuf.ByteString;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class VisionService {
+
+    // https://cloud.google.com/vision/docs/samples/vision-quickstart
+    public void detectLabelsInImages(String fileName){
+
+        try {
+            ImageAnnotatorClient vision = ImageAnnotatorClient.create();
+            Path path = Paths.get(fileName);
+            byte[] data = Files.readAllBytes(path);
+            ByteString imgBytes = ByteString.copyFrom(data);
+            List<AnnotateImageRequest> requests = new ArrayList<>();
+            Image img = Image.newBuilder().setContent(imgBytes).build();
+            Feature feat = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
+            AnnotateImageRequest request =
+                    AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+            requests.add(request);
+
+            // Performs label detection on the image file
+            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    System.out.format("Error: %s%n", res.getError().getMessage());
+                    return;
+                }
+
+                for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
+                    annotation
+                            .getAllFields()
+                            .forEach((k, v) -> System.out.format("%s : %s%n", k, v.toString()));
+
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    // https://cloud.google.com/vision/docs/samples/vision-localize-objects
+    public void detectLocalizedObjects(String filePath) throws IOException {
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+
+        ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        AnnotateImageRequest request =
+                AnnotateImageRequest.newBuilder()
+                        .addFeatures(Feature.newBuilder().setType(Feature.Type.OBJECT_LOCALIZATION))
+                        .setImage(img)
+                        .build();
+        requests.add(request);
+
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            // Perform the request
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            // Display the results
+            for (AnnotateImageResponse res : responses) {
+                for (LocalizedObjectAnnotation entity : res.getLocalizedObjectAnnotationsList()) {
+                    System.out.format("Object name: %s%n", entity.getName());
+                    System.out.format("Confidence: %s%n", entity.getScore());
+                    System.out.format("Normalized Vertices:%n");
+                    entity
+                            .getBoundingPoly()
+                            .getNormalizedVerticesList()
+                            .forEach(vertex -> System.out.format("- (%s, %s)%n", vertex.getX(), vertex.getY()));
+                }
+            }
+        }
+    }
+
+    public void detectDocumentText(String filePath) throws IOException {
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+
+        ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Feature.Type.DOCUMENT_TEXT_DETECTION).build();
+        AnnotateImageRequest request =
+                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+        requests.add(request);
+
+        // Initialize client that will be used to send requests. This client only needs to be created
+        // once, and can be reused for multiple requests. After completing all of your requests, call
+        // the "close" method on the client to safely clean up any remaining background resources.
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+            client.close();
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    System.out.format("Error: %s%n", res.getError().getMessage());
+                    return;
+                }
+
+                // For full list of available annotations, see http://g.co/cloud/vision/docs
+                TextAnnotation annotation = res.getFullTextAnnotation();
+                for (Page page : annotation.getPagesList()) {
+                    String pageText = "";
+                    for (Block block : page.getBlocksList()) {
+                        String blockText = "";
+                        for (Paragraph para : block.getParagraphsList()) {
+                            String paraText = "";
+                            for (Word word : para.getWordsList()) {
+                                String wordText = "";
+                                for (Symbol symbol : word.getSymbolsList()) {
+                                    wordText = wordText + symbol.getText();
+                                    System.out.format(
+                                            "Symbol text: %s (confidence: %f)%n",
+                                            symbol.getText(), symbol.getConfidence());
+                                }
+                                System.out.format(
+                                        "Word text: %s (confidence: %f)%n%n", wordText, word.getConfidence());
+                                paraText = String.format("%s %s", paraText, wordText);
+                            }
+                            // Output Example using Paragraph:
+                            System.out.println("%nParagraph: %n" + paraText);
+                            System.out.format("Paragraph Confidence: %f%n", para.getConfidence());
+                            blockText = blockText + paraText;
+                        }
+                        pageText = pageText + blockText;
+                    }
+                }
+                System.out.println("%nComplete annotation:");
+                System.out.println(annotation.getText());
+            }
+        }
+    }
+
+}
